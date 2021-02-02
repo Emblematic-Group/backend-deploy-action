1d17932b3236dd6325a5cf0fa526e9362a65c1b1
Marco Cavalli <cmarcos46@gmail.com>
2021-02-02 17:55:37 +0100

    Updated entrypoint for actions. Updated Dockerfile to run fewer commands

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM alpine:latest
+
+LABEL version="1.0.0"
+
+ENV GLIBC_VER=2.31-r0
+
+# install glibc compatibility for alpine
+RUN apk --no-cache add \
+        bash \
+    && apk add --update npm \
+    && apk add --update nodejs
+
+RUN npm install pm2@latest -g 
+ADD entrypoint.sh /entrypoint.sh
+RUN chmod 777 /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+echo "JOB STARTED"
+
+set -e
+
+echo "Check env variables STARTED"
+
+if [[ ! -z "$BACKEND_FOLDER" ]]; then
+    cd $AWS_FOLDER
+fi
+
+if [[ -z "$SSH_KEY_PEM" ]]; then
+    echo Missing ssh key
+    exit 1
+fi
+
+if [[ -z "$SSH_HOST" ]]; then
+    echo Missing ssh host
+    exit 1
+fi
+
+BRANCH_NAME="$(echo ${GITHUB_REF#refs/heads/})"
+
+if [[ "$BRANCH_NAME" != "awsstaging" && "$BRANCH_NAME" != "awsproduction" ]]; then
+    echo "Wrong branch. Only allowed for awsstaging and awsproduction"
+    exit 1
+fi
+
+mkdir -p ~/ssh-keys
+touch ~/ssh-keys/EmblematicReachServerSSHKey.pem
+echo "$SSH_KEY_PEM" > ~/ssh-keys/EmblematicReachServerSSHKey.pem
+chmod 400 ~/ssh-keys/EmblematicReachServerSSHKey.pem
+
+ssh -i ~/ssh-keys/EmblematicReachServerSSHKey.pem $SSH_HOST "[ -d ~/emblematic-github/${BRANCH_NAME} ] && echo true" > output.txt
+CHECK_LENGHT=$(cat output.txt)
+if [[ -z "$CHECK_LENGHT" ]]; then
+    echo "Folders not existing the branch ${BRANCH_NAME}. Creating them."
+	
+    if [[ "$BRANCH_NAME" == "awsstaging" ]]; then
+        pm2 deploy ecosystem.staging.config.js staging setup
+    fi
+
+    if [[ "$BRANCH_NAME" == "awsproduction" ]]; then
+        pm2 deploy ecosystem.production.config.js production setup
+    fi
+fi
+
+
+echo "Deploy/Update the backend with pm2"
+
+if [[ "$BRANCH_NAME" == "awsstaging" ]]; then
+    pm2 deploy ecosystem.staging.config.js staging --force
+fi
+
+if [[ "$BRANCH_NAME" == "awsproduction" ]]; then
+    pm2 deploy ecosystem.production.config.js production --force
+fi
+
+echo "Deployment is complete!"
