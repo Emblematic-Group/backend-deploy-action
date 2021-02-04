34b44ff761945dc54aa2d72a678a5e3825e30eb7
Marco Cavalli <cmarcos46@gmail.com>
2021-02-04 14:59:41 +0100

    This action will only be used to update/deploy the backend. Doesn't need to check the pm2 setup was done already

--- entrypoint.sh
+++ entrypoint.sh
@@ -34,22 +34,6 @@ touch ~/ssh-keys/EmblematicReachServerSSHKey.pem
 echo "$SSH_KEY_PEM" > ~/ssh-keys/EmblematicReachServerSSHKey.pem
 chmod 400 ~/ssh-keys/EmblematicReachServerSSHKey.pem
 
-echo "Check the existence of branch folders on remote pc"
-
-CHECK_LENGHT=$(ssh -o StrictHostKeyChecking=no -i ~/ssh-keys/EmblematicReachServerSSHKey.pem $SSH_HOST "[ -d ~/emblematic-github/${BRANCH_NAME} ] && echo true")
-if [[ -z "$CHECK_LENGHT" ]]; then
-    echo "Folders don't exist for the branch ${BRANCH_NAME}. Busy creting them."
-	
-    if [[ "$BRANCH_NAME" == "awsstaging" ]]; then
-        pm2 deploy ecosystem.staging.config.js staging setup
-    fi
-
-    if [[ "$BRANCH_NAME" == "awsproduction" ]]; then
-        pm2 deploy ecosystem.production.config.js production setup
-    fi
-fi
-
-
 echo "Deploy/Update the backend with pm2"
 
 if [[ "$BRANCH_NAME" == "awsstaging" ]]; then
